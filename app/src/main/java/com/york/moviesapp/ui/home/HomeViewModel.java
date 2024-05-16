dc68732839623d619c4fc16be4fb66550fef4b58
Arbnor Rama <arama@york.citycollege.eu>
2024-05-16 21:30:29 +0300

    Make homepage

--- app/src/main/java/com/york/moviesapp/ui/home/HomeViewModel.java
+++ app/src/main/java/com/york/moviesapp/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("York Movie App");
     }
 
     public LiveData<String> getText() {
