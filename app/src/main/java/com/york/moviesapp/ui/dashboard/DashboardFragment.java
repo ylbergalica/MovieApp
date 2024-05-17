f41a8120f93325d93c6ba8cdd94176e5e12536bd
GalicaYlber <galica.ylber@gmail.com>
2024-05-17 12:01:05 +0300

    Revert "Merge branch 'InternetCheck' into newProject/recycleView"
    This reverts commit ef01888abd8d19339b3f29cb28d4f1641577a782, reversing
    changes made to a3096414ad0463a55e6097d53037fd7b777b417f.

--- app/src/main/java/com/york/moviesapp/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/york/moviesapp/ui/dashboard/DashboardFragment.java
@@ -1,20 +1,11 @@
 package com.york.moviesapp.ui.dashboard;
 
-import static androidx.core.content.ContextCompat.getSystemService;
-
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkCapabilities;
-import android.net.NetworkInfo;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -51,20 +42,7 @@ private RecyclerView recyclerView;
 //        final TextView textView = binding.textDashboard;
 //        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
-        if (isNetworkAvailable(getContext())) {
-            // Here we fetch from the api because we have internet
-            Log.e("InternetCheck", "Network is not available");
-        }else {
-            // Here use tha cached data and show a message to the user
-
-            //create a toast message saying that there is no internet
-            Toast.makeText(getContext(), "No Internet Connection. Using cached data.", Toast.LENGTH_LONG).show();
-
-
-            Log.d("InternetCheck", "Network is available");
-        }
-
-        APIRequest.getDemoMovies(new APIRequest.ApiCallback() {
+        APIRequest.getMovies(new APIRequest.ApiCallback() {
             @Override
             public void onSuccess(JsonElement data) {
                 // Process the data here
@@ -85,32 +63,9 @@ private RecyclerView recyclerView;
         return root;
     }
 
-    private boolean isNetworkAvailable(Context context) {
-        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-
-        if (connectivityManager != null) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(connectivityManager.getActiveNetwork());
-                return networkCapabilities != null &&
-                        (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
-                                networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
-                                networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET));
-            } else {
-                NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-                return activeNetworkInfo != null && activeNetworkInfo.isConnected();
-            }
-        }
-        return false;
-    }
-
 @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
-
-
 }
-
-
-
