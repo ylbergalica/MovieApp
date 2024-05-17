33177cc652beff1eb181086c19dde7be110a66ba
Arbnor Rama <arama@york.citycollege.eu>
2024-05-17 12:10:23 +0300

    Fix internet check

--- app/src/main/java/com/york/moviesapp/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/york/moviesapp/ui/dashboard/DashboardFragment.java
@@ -1,11 +1,17 @@
 package com.york.moviesapp.ui.dashboard;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkCapabilities;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -42,25 +48,51 @@ private RecyclerView recyclerView;
 //        final TextView textView = binding.textDashboard;
 //        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
-        APIRequest.getMovies(new APIRequest.ApiCallback() {
-            @Override
-            public void onSuccess(JsonElement data) {
-                // Process the data here
-                Log.d("DATA", data.toString());
+        if(isNetworkAvailable(getActivity())) {
+            APIRequest.getMovies(new APIRequest.ApiCallback() {
+                @Override
+                public void onSuccess(JsonElement data) {
+                    // Process the data here
+                    Log.d("DATA", data.toString());
 
-                recyclerView.setAdapter(new MyRecyclerViewAdapter(data, getActivity(), DashboardFragment.this, getLayoutInflater()));
-            }
+                    recyclerView.setAdapter(new MyRecyclerViewAdapter(data, getActivity(), DashboardFragment.this, getLayoutInflater()));
+                }
 
 
-            @Override
-            public void onFailure(String errorMessage) {
-                // Handle failure
-                Log.e("APIRequest", errorMessage);
-            }
-        });
+                @Override
+                public void onFailure(String errorMessage) {
+                    // Handle failure
+                    Log.e("APIRequest", errorMessage);
+                }
+            });
+            Log.d("NETWORK", "Network is available");
+        } else {
+            Toast.makeText(getContext(), "No internet connection data is loaded from cache storage.", Toast.LENGTH_SHORT).show();
+            Log.d("NETWORK", "Network is not available");
+        }
+
+
 
 
         return root;
+    }
+
+    private boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (connectivityManager != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(connectivityManager.getActiveNetwork());
+                return networkCapabilities != null &&
+                        (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                                networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+                                networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET));
+            } else {
+                android.net.NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+                return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+            }
+        }
+        return false;
     }
 
 @Override
